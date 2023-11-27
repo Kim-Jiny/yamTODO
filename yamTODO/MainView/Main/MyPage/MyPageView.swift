53710f43c65ff1ef3e7f98342fde73155690fcc1
Kim-Jiny <kjinyz@naver.com>
2023-11-27 16:12:19 +0900

    스토어 버전 표기 추가

--- yamTODO/MainView/Main/MyPage/MyPageView.swift
+++ yamTODO/MainView/Main/MyPage/MyPageView.swift
@@ -13,7 +13,8 @@ import GoogleMobileAds
 struct MyPage: View {
     @ObservedObject var userColor: UserColorObject
 //    @StateObject var viewModel = ColorSettingViewModel()
-    
+    @State private var latestVersion: String = String(localized: "Store version Loading...")
+    @State private var isNewVersion: Bool = true
     @State private var isShowingMailView = false
     @State private var showFailedMailAlert = false
     @State private var result: Result<MFMailComposeResult, Error>?
@@ -26,25 +27,23 @@ struct MyPage: View {
         AdmobBannerView(adType: .subBN).frame(width: GADAdSizeBanner.size.width, height: GADAdSizeBanner.size.height)
     }
   
-  var body: some View {
-    NavigationView {
-      VStack {
-//        userInfo
-        
-        Form {
-            appInfoSection
-            taskInfoSection
-//            appSettingSection
-            forDeveloper
-            adSection
-//            admob()
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    appInfoSection
+                    taskInfoSection
+                    forDeveloper
+                    adSection
+                }
+            }
+                .navigationBarTitle("My Page")
         }
-      }
-      .navigationBarTitle("My Page")
-        
+        .navigationViewStyle(StackNavigationViewStyle())
+        .onAppear() {
+            checkForUpdate()
+        }
     }
-    .navigationViewStyle(StackNavigationViewStyle())
-  }
 }
 
 
@@ -52,10 +51,20 @@ private extension MyPage {
   // MARK: View
     var appInfoSection: some View {
       Section(header: Text("App settings").fontWeight(.medium)) {
-          if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-              Text("App version v\(appVersion)")
-                  .frame(height: 44)
+          if isNewVersion {
+              if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                  Text("App version v\(appVersion) 최신 버전 입니다.")
+                      .frame(height: 44)
+              }
+          }else {
+              if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                  NavigationLink(destination: ColorSettingMainView(userColor: userColor)) {
+                      Text("App version v\(appVersion) 최신 버전이 아닙니다.")
+                          .frame(height: 44)
+                  }
+              }
           }
+          
           NavigationLink(destination: ColorSettingMainView(userColor: userColor)) {
               Text("Setting App Color")
                   .frame(height: 44)
@@ -101,26 +110,44 @@ private extension MyPage {
     }
     
     func scheduleNotification() {
-            let content = UNMutableNotificationContent()
-            content.title = "알림 제목"
-            content.body = "알림 본문"
-            content.sound = UNNotificationSound.default
+        let content = UNMutableNotificationContent()
+        content.title = "알림 제목"
+        content.body = "알림 본문"
+        content.sound = UNNotificationSound.default
 
-            // 알림이 표시되는 시간 설정 (예: 10초 후)
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        // 알림이 표시되는 시간 설정 (예: 10초 후)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
 
-            // 알림 요청 생성
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        // 알림 요청 생성
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
 
-            // 요청을 notification center에 추가
-            UNUserNotificationCenter.current().add(request) { error in
-                if let error = error {
-                    print("Error adding notification request: \(error.localizedDescription)")
-                } else {
-                    print("Notification request added successfully")
+        // 요청을 notification center에 추가
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error adding notification request: \(error.localizedDescription)")
+            } else {
+                print("Notification request added successfully")
+            }
+        }
+    }
+    
+    func checkForUpdate() {
+        AppVersionCheck.isUpdateAvailable { version in
+            if let version = version {
+                // 비동기로 가져온 최신 버전을 UI에 반영하기 위해 메인 스레드에서 업데이트
+                DispatchQueue.main.async {
+                    self.latestVersion = String(localized: "Store version v\(version)")
+                }
+                
+                isNewVersion = AppVersionCheck.isNewVersion(version)
+            } else {
+                // 최신 버전을 가져오는 데 실패한 경우, 에러 처리 또는 기본값 표시 등을 수행
+                DispatchQueue.main.async {
+                    self.latestVersion = "Failed to fetch version"
                 }
             }
         }
+    }
 }
 
 extension ContentView {
@@ -134,6 +161,7 @@ extension ContentView {
         return Coordinator()
     }
 }
+
 extension Result {
     var isSuccess: Bool {
         if case .success = self {
@@ -146,3 +174,63 @@ extension Result {
         return !isSuccess
     }
 }
+
+class AppVersionCheck {
+    // 코드작성
+    static func isUpdateAvailable(completion: @escaping (String?) -> Void) {
+        guard let info = Bundle.main.infoDictionary,
+            let currentVersion = info["CFBundleShortVersionString"] as? String,
+            let identifier = info["CFBundleIdentifier"] as? String,
+            let url = URL(string: "http://itunes.apple.com/kr/lookup?id=6472643559") else {
+                completion(nil)
+                return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let error = error { throw error }
+                guard let data = data else {
+                    completion(nil)
+                    return
+                }
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
+                if let results = json?["results"] as? [Any],
+                   let result = results.first as? [String: Any],
+                   let version = result["version"] as? String {
+                    completion(version)
+                } else {
+                    completion(nil)
+                }
+            } catch {
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+    
+    static func isNewVersion(_ storeV: String) -> Bool {
+        let verFloat = NSString.init(string: storeV).floatValue
+        guard let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return true
+        }
+        let currentVerFloat = NSString.init(string: currentVersion).floatValue
+        
+        return verFloat >= currentVerFloat
+    }
+    
+    func appUpdate() {
+        let appleId = "6472643559"        // 앱 스토어에 일반 정보의 Apple ID 입력
+        // UIApplication 은 Main Thread 에서 처리
+        DispatchQueue.main.async {
+            if let url = URL(string: "itms-apps://itunes.apple.com/app/\(appleId)"), UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+    }
+}
+
